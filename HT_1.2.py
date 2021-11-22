87a1d855c69c5170941635ab3d968b1031e861a0
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2021-11-22 12:36:03 +0100

    New HT_1.2

--- HT_1.2.py
+++ HT_1.2.py
@@ -1,5 +1,13 @@
-color_list_1 = set(["White", "Black", "Red"])
-color_list_2 = set(["Red", "Green"])
-for i in list(color_list_2):
-    color_list_1.discard(i)
-print(color_list_1)
+color_list_1 = list(input("Write first group of colors using only comma: ").split(","))
+color_list_2 = list(input("Write second group of colors using only comma: ").split(","))
+
+#color_list_1 = set(["White", "Black", "Red"])
+#color_list_2 = set(["Red", "Green"])
+#for i in list(color_list_2):
+#    color_list_1.discard(i)
+result_list= []
+
+for i in color_list_1:
+    if i not in color_list_2:
+        result_list.append(i)
+print(result_list)
